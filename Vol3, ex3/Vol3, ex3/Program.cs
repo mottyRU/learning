ee6b3580e445c3a5be2c106f6d02620ae397faab
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-10-24 20:47:11 +0300

    Hello

---
+++ Vol3, ex3/Vol3, ex3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vol3__ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int A, number;
+                A = 1;
+                number = 0;
+                while (A != 0)
+                {
+                    A = Int32.Parse(Interaction.InputBox("Enter number:"));
+                    number += A;
+                    MessageBox.Show("Result is:" + number + "");
+                }
+                MessageBox.Show("Wrong number!");
+            }
+            catch
+            {
+                MessageBox.Show("Error!");
+            }
+        }
+    }
+}
