5072ebdec01bee274423386977dd83f65349c8e1
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-10-27 18:13:04 +0300

    goal 8

---
+++ Глава_3(задача_8)/Задача 3.8/Задача 3.8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_3._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, n;
+            A = B = 1;
+            C = A + B;
+            string step;
+            Console.WriteLine("Enter step number:");
+            for (n = Int32.Parse(Console.ReadLine()); n == 0; n--)
+            {
+                A = B = 1;
+                C = A + B;
+                B = (A + B);
+                A = B;
+                Console.WriteLine("Result is: "+A, B, C+"");
+            }
+            Console.ReadKey();
+        }
+    }
+}
