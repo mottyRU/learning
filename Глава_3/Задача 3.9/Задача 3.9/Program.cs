e720d81ee5b30019bbc6bf5855b5557ab5410056
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-10-28 11:38:31 +0300

    задача 9

---
+++ Глава_3/Задача 3.9/Задача 3.9/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_3._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+            Console.WriteLine("Enter A:");
+            A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter B:");
+            B = Convert.ToInt32(Console.ReadLine());
+            if (A >= B)
+            {
+                if (A == B)
+                {
+                    Console.WriteLine("Error! A = B");
+                }
+                else
+                {
+                    while (A == B)
+                    {
+                        B = B++;
+                        Console.WriteLine("" + B + "");
+                    }
+                    Console.WriteLine("End of program.");
+                }                
+            }
+            else
+            {
+                do
+                {
+                    A++;
+                }
+                while (A == B);
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- Глава_3/Задача 3.9/Задача 3.9/Properties/AssemblyInfo.cs
+++ Глава_3/Задача 3.9/Задача 3.9/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Vol3, ex3")]
+[assembly: AssemblyTitle("Задача 3.9")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vol3, ex3")]
+[assembly: AssemblyProduct("Задача 3.9")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("4b89702e-96e8-4263-ab39-43bb1bc25ae9")]
+[assembly: Guid("aa4865a7-e341-4ca1-b7ee-7b3ab0c7c3e9")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
