d0aea5aec7231135b8f176f7f7010ecad0e1bc10
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-10-28 17:51:39 +0300

    10

---
+++ Глава_3/Задача 3.10/Задача 3.10/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_3._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, A, B, C;
+            A = 1;
+            
+            Console.WriteLine("Enter n:");
+            for (n = Int32.Parse(Console.ReadLine()); n != 0; n--)
+            {
+                A++;
+                //Console.WriteLine("" + A + "");
+                if ((A % 5==2)&(A % 3==1))
+                {
+                    A +=A;
+                    
+                }
+            }
+            Console.WriteLine("Summa:" + A + "");
+            Console.ReadKey();
+        }
+    }
+}
+/* int A, B, C, n;
+            A = B = 1;
+            C = A + B;
+            string step;
+            Console.WriteLine("Enter step number:");
+            for (n = Int32.Parse(Console.ReadLine()); n == 0; n--)
+            {
+                A = B = 1;
+                C = A + B;
+                B = (A + B);
+                A = B;
+                Console.WriteLine("Result is: "+A, B, C+"");
+            }
+            Console.ReadKey();*/
