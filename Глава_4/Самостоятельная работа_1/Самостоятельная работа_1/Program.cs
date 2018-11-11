3bdf77b18e628848aed80f904af87584db20a168
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-11-11 17:19:40 +0300

    hello

---
+++ Глава_4/Самостоятельная работа_1/Самостоятельная работа_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Самостоятельная_работа_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                int[] numbers;
+                Console.WriteLine("Enter massive number:");
+                numbers = new int[Int32.Parse(Console.ReadLine())];
+                Console.WriteLine("Massive of numbers:");
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (i % 5 == 2)
+                    {
+                        Console.WriteLine("" + i + "");
+                    }
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("Error! Enter number!");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
