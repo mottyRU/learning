bd581080114617976e612a421bf6ef162bc6c4e3
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-11-13 21:32:39 +0300

    3

---
+++ Глава_4/Самостоятельная_работа_3/Самостоятельная_работа_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Самостоятельная_работа_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] symb;
+            int size = 10;
+            symb = new char[size];
+            symb[0] = 'a';
+            Console.WriteLine("Massive:");
+            for (int i = 0; i<symb.Length; i++)
+            {
+                symb[i] =(char) (i+1);
+                Console.WriteLine(""+symb[i]+"");
+            }
+            Console.ReadKey();
+        }
+    }
+}
