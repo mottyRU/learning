78b113c180acadb49f4356715a657b06d0be1c40
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-11-13 21:34:10 +0300

    33

--- Глава_4/Самостоятельная_работа_3/Самостоятельная_работа_3/Program.cs
+++ Глава_4/Самостоятельная_работа_3/Самостоятельная_работа_3/Program.cs
@@ -17,7 +17,7 @@ namespace Самостоятельная_работа_3
             Console.WriteLine("Massive:");
             for (int i = 0; i<symb.Length; i++)
             {
-                symb[i] =(char) (i+1);
+                symb[i] =(int) symb[i]+1;
                 Console.WriteLine(""+symb[i]+"");
             }
             Console.ReadKey();
