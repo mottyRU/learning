f81fd74c023777f7a798e3bf6b6d5881e0ccf17a
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-11-15 19:35:01 +0300

    4.1

--- Глава_4/Самостоятельная_работа_4/Самостоятельная_работа_4/Program.cs
+++ Глава_4/Самостоятельная_работа_4/Самостоятельная_работа_4/Program.cs
@@ -10,44 +10,20 @@ namespace Самостоятельная_работа_4
     {
         static void Main(string[] args)
         {
-            int size = 10; ;
+            int size = 10;
             char[] symb = new char[size];
             Console.WriteLine("Char Massive:");
+            symb[0] = 'A';
             for (int i=0; i<symb.Length-1; i++)
-            {
-                symb[0] = 'A';
-                symb[i+1] = (char) ((int)symb[i] + 1);
-                Console.Write("" + symb[i] + ""+" ");
-                
+            {                
+                symb[i+1] = (char) (symb[i] + 1);                             
             }
+            foreach (char i in symb)
+            {
+                Console.Write("" + i + "" + " ");
+            }
+            
             Console.WriteLine();
-            switch(symb[size])
-            {
-                case 'A':
-                    break;
-                case 'B':
-                    Console.Write("B" + " ");
-                    break;
-                case 'C':
-                    Console.Write("C" + " ");
-                    break;
-                case 'D':
-                    Console.Write("D" + " ");
-                    break;
-                case 'E':
-                    break;
-                case 'F':
-                    Console.Write("F" + " ");
-                    break;
-                case 'G':
-                    Console.Write("G" + " ");
-                    break;
-                case 'H':
-                    Console.Write("H" + " ");
-                    break;
-                case 'I':
-                    break;
-            }
             Console.ReadKey();
         }
     }
