f96f0db549d153cb0907dcf3949bb282a7eaa59b
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-11-15 19:47:24 +0300

    4.2

--- Глава_4/Самостоятельная_работа_4/Самостоятельная_работа_4/Program.cs
+++ Глава_4/Самостоятельная_работа_4/Самостоятельная_работа_4/Program.cs
@@ -13,16 +13,24 @@ namespace Самостоятельная_работа_4
             int size = 10;
             char[] symb = new char[size];
             Console.WriteLine("Char Massive:");
-            symb[0] = 'A';
+            symb[0] = 'B';
             for (int i=0; i<symb.Length-1; i++)
             {                
-                symb[i+1] = (char) (symb[i] + 1);                             
+                symb[i+1] = (char) (symb[i] + 1);
+                if (symb[i]=='D')
+                {
+                    symb[i+1] = (char)(symb[i] + 2);
+                }
+                if (symb[i+1] == 'I')
+                {
+                    symb[i + 1] = (char)(symb[i] + 2);
+                }
             }
             foreach (char i in symb)
             {
-                Console.Write("" + i + "" + " ");
+                Console.Write("" + i + " ");
             }
-            
+                       
             Console.WriteLine();
             Console.ReadKey();
         }
