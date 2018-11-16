056564afa762a39a856f8a66c3ce5b2a3db5ccb7
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-11-16 19:56:28 +0300

    5.2

--- Глава_4/Самостоятельная работа_5/Самостоятельная работа_5/Program.cs
+++ Глава_4/Самостоятельная работа_5/Самостоятельная работа_5/Program.cs
@@ -13,27 +13,33 @@ namespace Самостоятельная_работа_5
             int size = 10;
             int[] numb = new int[size];
             Random rnd = new Random();
+            int value, index = 0;
+            value = numb[index];
             Console.WriteLine("Massive:");
             for (int i = 0; i < numb.Length; i++)
             {
                 numb[i] = rnd.Next(1, 10);
             }
-            foreach (int i in numb)
+            foreach (int r in numb)
             {
-                Console.Write(i + " ");
+                if (r < value)
+                {
+                    value = r;
+                }
+                Console.Write(r + " ");
             }
-            Console.WriteLine("Sort Massive:");
-            int value, index = 0;
-            value = numb[index];
+            Console.WriteLine();
+            Console.WriteLine("Min number: " + value);
+            //Console.WriteLine("Sort Massive:");
+            
             for (int i = 0; i < numb.Length; i++)
             {
                 if(numb[i]<value)
                 {
                     value = numb[i];
-                    index = i;
+                    //index = i;
                 }             
-            }
-            Console.WriteLine("Min number: " + value);
+            }               
             Console.WriteLine("Index: " + index);
             Console.ReadKey();
         }
