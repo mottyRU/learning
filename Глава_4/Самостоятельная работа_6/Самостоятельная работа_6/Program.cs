bfafdb769e2c9a1d62ff673927b893eb04fc9c1a
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-11-17 12:18:29 +0300

    4.6

---
+++ Глава_4/Самостоятельная работа_6/Самостоятельная работа_6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Самостоятельная_работа_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 10;
+            int[] numb = new int[size];
+            Random rnd = new Random();
+            Console.WriteLine("Massive:");
+            for (int i = 0; i<numb.Length; i++)
+            {
+                numb[i] = rnd.Next(1,20);
+            }
+            foreach (int s in numb)
+            {
+                Console.Write(s + " ");
+            }
+            int NewNumber;
+            for (int i = 1; i < numb.Length; i++)
+            {
+                for (int j = 0; j < numb.Length-i; j++)
+                {
+                    if (numb[j]<numb[j+1])
+                    {
+                        NewNumber = numb[j + 1];
+                        numb[j + 1] = numb[j];
+                        numb[j] = NewNumber;
+                    }
+                }            
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sort Massive:");
+            foreach (int s in numb)
+            {
+                Console.Write(s + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
