641fd79a66bc686e474d01d92d419ac11c06f4fc
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-11-18 18:18:08 +0300

    new

--- Глава_4/Самостоятельная работа_9/Самостоятельная работа_9/Program.cs
+++ Глава_4/Самостоятельная работа_9/Самостоятельная работа_9/Program.cs
@@ -10,7 +10,7 @@ namespace Самостоятельная_работа_9
     {
         static void Main(string[] args)
         {
-            int[,] numb = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int[,] numb = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
             Console.WriteLine("Massive:");
             for (int i = 0; i < numb.GetLength(0); i++)
             {
@@ -21,22 +21,20 @@ namespace Самостоятельная_работа_9
                 Console.WriteLine();
             }            
             Random rnd = new Random();
-            int row = 2;
-            int col = 3;
-            //int row = rnd.Next(numb.GetLength(0));
-            //int col = rnd.Next(numb.GetLength(1));
+            int row = rnd.Next(numb.GetLength(0)+1);
+            int col = rnd.Next(numb.GetLength(1)+1);
             Console.WriteLine("Delete {0} row and {1} col", row, col);
             Console.ReadKey();
             int A, B;
-            int[,] NewMassive = new int [numb.GetLength(0)-1, numb.GetLength(1)-1];
-            for (int i = 1; i<numb.GetLength(0); i++)
+            int[,] NewMassive = new int [numb.GetLength(0)+1, numb.GetLength(1)+1];
+            for (int i = 0; i<numb.GetLength(0); i++)
             {
-                if (i > row) A = i-1;
-                else A = i;
-                for (int j = 1; j<numb.GetLength(1);i++)
+                if (i < row) A = i;
+                else A = i+1;
+                for (int j = 0; j<numb.GetLength(1);i++)
                 {
-                    if (j > col) B = j-1;
-                    else B = j;
+                    if (j < col) B = j;
+                    else B = j+1;
                     NewMassive[A, B] = numb[i, j];
                 }
             }
