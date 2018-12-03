bd81c72389be27360065d7d52aa779966da58191
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-03 18:35:51 +0300

    hello

---
+++ Глава_6/Задача_1/Задача_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1
+{
+    class Program
+    {
+        private int number;
+        
+        public void Prisvoit()
+        {
+            Console.WriteLine("Enter number: ");
+            number = Int32.Parse(Console.ReadLine());
+        }
+        public void Symbol()
+        {
+            
+            Console.WriteLine((char) ((int) number));
+        }
+        public void SymbolAndNumber()
+        {
+            Console.WriteLine(""+number+","+ (char)((int)number));
+        }
+    }
+    class MyClass
+    {
+        static void Main()
+        {
+            Program B = new Program();
+            B.Prisvoit();
+            B.Symbol();
+            B.SymbolAndNumber();
+            Console.ReadKey();
+        }
+    }
+}
