af6bc70124be9dbadd114de3ceae6f32784b6860
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-05 21:02:21 +0300

    5

---
+++ Глава_6/Задача_5/Задача_5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_5
+{
+    class MyClass
+    {
+        private int Number;
+        public void Prisvoit()
+        {
+            Number = 0;
+        }
+        public void Prisvoit(int n)
+        {
+            if (n > 100)
+            {
+                Number = 100;
+            }
+            else
+            {
+                Number = n;
+            }
+        }
+        public void Show()
+        {
+            Console.WriteLine(Number);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass A = new MyClass();
+            Random rnd = new Random();
+            A.Prisvoit();
+            A.Show();
+            A.Prisvoit(rnd.Next(90, 110));
+            A.Show();
+            Console.ReadKey();
+        }
+    }
+}
