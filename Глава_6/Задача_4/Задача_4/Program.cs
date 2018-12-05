b50ae1f14231c06166f9ab672016b539f7d4a1d9
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-05 20:30:19 +0300

    44444

--- Глава_6/Задача_4/Задача_4/Program.cs
+++ Глава_6/Задача_4/Задача_4/Program.cs
@@ -18,12 +18,22 @@ namespace Задача_4
         public MyClass(double K)
         {
             string[] Text;
+            string ProverkaZnakov;
             Text = Convert.ToString(K).Split((new Char[] {'.', ','}));
             Console.WriteLine(Text[0]);
             Console.WriteLine(Text[1]);
+            if (Text[1].Length == 0)
+            {
+                ProverkaZnakov = "0";
+            }
+            else if (Text[1].Length == 1)
+            {
+                ProverkaZnakov = Text[1][0] + "";
+            }
+            else (ProverkaZnakov == Text[1][0] + Text[1][1] + "");
             Console.ReadKey();
             Symbol =(char) Convert.ToInt32(Text[0]);
-            Number = Convert.ToInt32(Text[1]);
+            Number = Convert.ToInt32(ProverkaZnakov);
 
         }
         public void Show()
@@ -38,7 +48,7 @@ namespace Задача_4
             Random rnd = new Random();
             MyClass A = new MyClass('A', 55);
             A.Show();
-            MyClass B = new MyClass(2*rnd.NextDouble()+70);
+            MyClass B = new MyClass(100*2*rnd.NextDouble());
             B.Show();
             Console.ReadKey();            
         }
