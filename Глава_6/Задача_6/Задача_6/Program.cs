a660028f1ade44093ee842ba99c98a3612128f1b
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-07 20:49:26 +0300

    6

---
+++ Глава_6/Задача_6/Задача_6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_6
+{
+    class MyClass
+    {
+        private int Max, Min;
+        public void Prisvoit(int n)
+        {
+            int Hranitel;
+            Random rnd = new Random();
+            Max = rnd.Next(100);
+            Min = rnd.Next(100);
+            if (Min > Max)
+            {
+                Hranitel = Max;
+                Max = Min;
+                Min = Hranitel;
+            }
+            Console.WriteLine(""+n+", "+Max+", "+Min+"");
+            if (Min > Max)
+            {
+                Hranitel = Max;
+                Max = Min;
+                Min = Hranitel;
+            }
+            else if (n > Max)
+            {
+                Max = n;
+            }
+        }
+        public void Prisvoit(int n, int m)
+        {
+
+        }
+        public void Show()
+        {
+            Console.WriteLine(""+Max+", "+Min+"");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            MyClass A = new MyClass();
+            A.Prisvoit(rnd.Next(100));
+            A.Show();
+            Console.ReadKey();
+        }
+    }
+}
