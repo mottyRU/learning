6f7ebdcc73d5a4a5831097c12508e81b8de1fa73
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-07 21:27:12 +0300

    6.1

--- Глава_6/Задача_6/Задача_6/Program.cs
+++ Глава_6/Задача_6/Задача_6/Program.cs
@@ -9,33 +9,35 @@ namespace Задача_6
     class MyClass
     {
         private int Max, Min;
+        public MyClass(int n, int m)
+        {
+            if (n > m)
+            {
+                Max = n;
+                Min = m;
+            }
+            else if (m > n)
+            {
+                Max = m;
+                Min = n;
+            }
+        }
         public void Prisvoit(int n)
         {
             int Hranitel;
-            Random rnd = new Random();
-            Max = rnd.Next(100);
-            Min = rnd.Next(100);
-            if (Min > Max)
-            {
-                Hranitel = Max;
-                Max = Min;
-                Min = Hranitel;
-            }
             Console.WriteLine(""+n+", "+Max+", "+Min+"");
-            if (Min > Max)
+            if (n > Max)
             {
                 Hranitel = Max;
-                Max = Min;
-                Min = Hranitel;
-            }
-            else if (n > Max)
-            {
                 Max = n;
+                n = Hranitel;
             }
-        }
-        public void Prisvoit(int n, int m)
-        {
-
+            else if (n < Min)
+            {
+                Hranitel = Min;
+                Min = n;
+                n = Hranitel;
+            }
         }
         public void Show()
         {
@@ -44,10 +46,10 @@ namespace Задача_6
     }
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Random rnd = new Random();
-            MyClass A = new MyClass();
+            MyClass A = new MyClass(rnd.Next(100), rnd.Next(100));
             A.Prisvoit(rnd.Next(100));
             A.Show();
             Console.ReadKey();
