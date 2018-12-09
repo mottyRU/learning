76b131304785ac6436c9ecb14e8529473b44843b
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-09 20:16:41 +0300

    qwerty

--- Глава_6/Задача_9/Задача_9/Program.cs
+++ Глава_6/Задача_9/Задача_9/Program.cs
@@ -10,6 +10,27 @@ namespace Задача_9
     {
         class MyClass
         {
+            public MyClass (int[] n)
+            {
+                int Hranitel;
+                for (int i = 1; i < n.Length; i++)
+                {
+                    for (int j = 0; j < n.Length - 1; j++)
+                    {
+                        if (n[j] > n[j + 1])
+                        {
+                            Hranitel = n[j + 1];
+                            n[j + 1] = n[j];
+                            n[j] = Hranitel;
+                        }
+                    }
+                }
+                for (int i = 0; i < n.Length; i++)
+                {
+                    Console.Write("| " + n[i] + " ");
+                }
+                Console.WriteLine("|");
+            }
             public static void MethodMaximum(int [] n)
             {
                 int index = 0;
@@ -36,27 +57,28 @@ namespace Задача_9
                 }
                 Console.WriteLine("Min value is: " + value);
             }
-            public static void MethodSeredina(int[] n)
+            public static void MethodMediana(int[] n)
             {
-                int Summa = n.Sum();
-                int index = 0;
-                int Mediana = n[index];
-                for (int i = 0; i < n.Length; i++)
+                int A, B;
+                if (n.Length%2!=0)
                 {
-                    Mediana += n[i];
-                    if (Mediana >= Summa / 2)
-                    {
-                        Mediana = n[i];
-                    }
+                    A = n.Length / 2;
+                    B = n[A];
+                    Console.WriteLine("Mediana value is: " + B);
                 }
-                Console.WriteLine("Mediana value is: " + Mediana);
+                else
+                {
+                    A = (n.Length / 2);
+                    B = A + (A - 1) / 2;
+                    Console.WriteLine("Mediana value is: " + B);
+                }
             }
         }
         static void Main()
         {
             Random rnd = new Random();
-            //int size = rnd.Next(5);
-            int size = 5;
+            //int size = rnd.Next(10);
+            int size = 6;
             int[] Numbers = new int[size];
             for (int i = 0; i < Numbers.Length; i++)
             {
@@ -64,9 +86,10 @@ namespace Задача_9
                 Console.Write("| " + Numbers[i] + " ");
             }
             Console.WriteLine("|");
+            MyClass A = new MyClass(Numbers);
             //MyClass.MethodMaximum(Numbers);
             //MyClass.MethodMinimum(Numbers);
-            MyClass.MethodSeredina(Numbers);
+            MyClass.MethodMediana(Numbers);
             Console.ReadKey();
         }
     }
