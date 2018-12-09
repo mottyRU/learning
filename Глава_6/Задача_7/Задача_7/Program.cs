14ccad0c0d49e7c9e01e2a4c2c29924b404dee3f
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-09 16:05:39 +0300

    задача 7

---
+++ Глава_6/Задача_7/Задача_7/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_7
+{
+    class MyClass
+    {
+        public char Symbol;
+        public string Text;
+        char[] TextMassive = new char [2];
+        /*public MyClass()
+        {
+            Random rnd = new Random();
+            int size = rnd.Next(1, 5);
+            char[] TextMassive = new char[size];
+            for (int i = 0; i < TextMassive.Length; i++)
+            {
+                TextMassive[i] = (char) rnd.Next(100);
+            }
+        }*/
+        public void Prisvoit(char n)
+        {
+            Symbol = n;
+        }
+        public void Prisvoit(string n)
+        {
+            Text = n;
+        }
+        public void Prisvoit(char [] n)
+        {
+            if (n.Length == 1)
+            {
+                Symbol = n[0];
+            }
+            else
+            {
+                Text = "" + n;
+            }
+        }
+        public void Show()
+        {
+            Console.WriteLine("" + Text+" " + Symbol+" " + TextMassive);
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            //Random rnd = new Random();
+            //int size = rnd.Next(1, 5);
+            //char[] TextMassive = new char[size];
+            MyClass A = new MyClass();            
+            A.Prisvoit();
+            A.Show();
+            Console.ReadKey();
+        }
+    }
+}
