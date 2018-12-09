187db004059d8fd75a945d54106e2c6d67675a39
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-09 17:19:36 +0300

    задача 8

---
+++ Глава_6/Задача_8/Задача_8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_8
+{
+    class MyClass
+    {
+        private static int Number = 0;
+        public static void Show()
+        {
+            Console.WriteLine(Number);
+            Number++;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass.Show();
+            MyClass.Show();
+            Console.ReadKey();
+        }
+    }
+}
