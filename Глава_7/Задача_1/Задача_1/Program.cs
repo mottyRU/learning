5c066c3f916d895cef67becd7cbaeba5f4fa5b78
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-12 20:56:05 +0300

    111

--- Глава_7/Задача_1/Задача_1/Program.cs
+++ Глава_7/Задача_1/Задача_1/Program.cs
@@ -8,23 +8,22 @@ namespace Задача_1
 {
     class Program
     {
-        public static string Probel(string text)
+        public static string Probel(string n)
         {
-            string NewText;
-            for (int i = 0; i < text.Length; i++)
+            for (int i = 0; i < n.Length; i++)
             {
-                text.Insert(i, " ");
+                n.Insert(i, " ");
             }
-            NewText = text;
-            Console.WriteLine(NewText);
-            return NewText;
+            return n;
         }
         static void Main()
         {
+            string NewText;
             string text = "Hello world!";
             Console.WriteLine("Значение до: " + text);
             Probel(text);
-            //Console.WriteLine("Значение после: " + NewText);
+            NewText = Probel(n);
+            Console.WriteLine("Значение после: " + m);
             Console.ReadKey();
         }
     }
