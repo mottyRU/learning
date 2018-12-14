06bb28a4290b3d53043af20d3451d910b935638e
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-14 19:16:10 +0300

    задача 2

---
+++ Глава_7/Задача_7/Задача_7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_7
+{
+    class Program
+    {
+        public static string Reversed(string n)
+        {
+            string[] Revers = n.Split();
+            string str ="";
+            for (int i = Revers.Length-1; i >= 0 ; i--)
+            {
+                char[] A = Revers[i].ToCharArray();
+                foreach (char s in A)
+                {
+                    str = s + str;
+                }
+                str = " " + str;
+            }
+            return str;
+        }
+        static void Main()
+        {
+            string Text = "Hello Wild World", NewText;
+            NewText = Reversed(Text);
+            Console.WriteLine("Текст до: " + Text);
+            Console.WriteLine("Текст после: " + NewText);
+            Console.ReadKey();
+        }
+    }
+}
