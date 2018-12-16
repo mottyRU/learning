a0b6921a8c965fdd7f2c6e092a07751829c492b8
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-16 17:00:31 +0300

    5

--- Глава_7/Задача_5/Задача_5/Program.cs
+++ Глава_7/Задача_5/Задача_5/Program.cs
@@ -12,27 +12,30 @@ namespace Задача_5
         {
             int start = 0;
             int at = 0;
+            int i = 0;
             int end = n.Length;
-            int[] NewMassive = new int[n.Length];
-            Console.Write("The string '{0}' occurs at position(s): ", m);
+            int[] NewMassive = new int[end];
             while (start <= end)
             {
                 at = n.IndexOf(m, start);
                 if (at == -1) break;
-                //Console.Write("{0} ", at);
-                NewMassive[n[end-1]] = at;
-                end = end - 1;
-                start = at + 1;
-                Array.Resize(ref NewMassive, at);
+                NewMassive[i] = at;                
+                i += 1;
+                start = at + 1;                
             }
-            Console.WriteLine();
+            Array.Resize(ref NewMassive, i);
             return NewMassive;
         }
         static void Main()
         {
             string Text = "Hello Wild World";
             char ForSearch = 'l';            
-            Console.WriteLine(IndexSearch(Text, ForSearch));
+            Console.WriteLine("Array line: ");
+            int[] Hranitel = IndexSearch(Text, ForSearch);
+            for (int i = 0; i < Hranitel.Length; i++)
+            {
+                Console.WriteLine(Hranitel);
+            }
             Console.ReadKey();
         }
     }
