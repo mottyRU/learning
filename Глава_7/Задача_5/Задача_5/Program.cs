a6437e0cbc4087f64bf500a768b5b9ad2e18830b
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-16 15:01:13 +0300

    5

---
+++ Глава_7/Задача_5/Задача_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_5
+{
+    class Program
+    {
+        public static string IndexSearch(string n, char m)
+        {
+            int index;
+            string A;
+            index = n.IndexOf(m);
+            A = "" + index;
+            return A;
+        }
+        static void Main()
+        {
+            string Text = "Hello Wild World";
+            char ForSearch = 'W';            
+            Console.WriteLine("Search result is: {0}", IndexSearch(Text, ForSearch));
+            Console.ReadKey();
+        }
+    }
+}
