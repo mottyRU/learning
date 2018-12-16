1d670b6a038149ca7a5a741cfd826fc331d64a58
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-16 15:44:32 +0300

    5

--- Глава_7/Задача_5/Задача_5/Program.cs
+++ Глава_7/Задача_5/Задача_5/Program.cs
@@ -8,19 +8,31 @@ namespace Задача_5
 {
     class Program
     {
-        public static string IndexSearch(string n, char m)
+        public static int IndexSearch(string n, char m)
         {
-            int index;
-            string A;
-            index = n.IndexOf(m);
-            A = "" + index;
-            return A;
+            int start;
+            int at = 0;
+            int end;
+            int count = 0;
+            end = n.Length;
+            start = 0;
+            Console.Write("The string 'l' occurs at position(s): ");
+            while ((start <= end) && (at > -1))
+            {
+                count = end - start;
+                at = n.IndexOf("l", start, count);
+                if (at == -1) break;
+                Console.Write("{0} ", at);
+                start = at + 1;
+            }
+            Console.WriteLine();
+            return at;
         }
         static void Main()
         {
             string Text = "Hello Wild World";
             char ForSearch = 'W';            
-            Console.WriteLine("Search result is: {0}", IndexSearch(Text, ForSearch));
+            IndexSearch(Text, ForSearch);
             Console.ReadKey();
         }
     }
