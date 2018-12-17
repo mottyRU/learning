781854f4593c1a864ba193e438f183c58077d8b9
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-17 19:34:16 +0300

    8

---
+++ Глава_7/Задача_8/Задача_8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_8
+{
+    class MyClass
+    {
+        public string TextPole;
+        public string Vstavka(string n, int m)
+        {
+            return TextPole.Insert(m, n);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass A = new MyClass();
+            A.TextPole = "Hello_Wild_World";
+            Console.WriteLine(A.Vstavka("MUR", 11));
+            Console.ReadKey();
+        }
+    }
+}
