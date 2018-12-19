26440ad465a8447ff54a30eac4d584eaca73cb61
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-19 19:39:32 +0300

    q

---
+++ Глава_7/Задача_10/Задача_10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_10
+{
+    class MyClass
+    {
+        public ref int[] Numbers;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Alpha = new int[5];
+        }
+    }
+}
