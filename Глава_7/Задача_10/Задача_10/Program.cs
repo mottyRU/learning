b86d47fb372c9728daeca708377089b6afe09d14
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-21 19:34:00 +0300

    q

--- Глава_7/Задача_10/Задача_10/Program.cs
+++ Глава_7/Задача_10/Задача_10/Program.cs
@@ -8,13 +8,38 @@ namespace Задача_10
 {
     class MyClass
     {
-        public ref int[] Numbers;
+        public int[] Numbers = new int[10];
+        public MyClass()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Numbers[i] = rnd.Next(50);
+            }
+        }
+        public override string ToString()
+        {
+            string Txt = "";
+            int Summa = 0;
+            foreach (int s in Numbers)
+            {
+                Summa += s; 
+            }
+            Txt += "Значения массива: " + String.Join(".", Numbers);
+            Txt += "\nКоличество элементов в массиве: " + Numbers.Length;
+            Txt += "\nСреднее значение элементов массива: " + Convert.ToString(Summa / Numbers.Length);
+            return Txt;
+        }
+
     }
     class Program
     {
         static void Main(string[] args)
         {
-            int[] Alpha = new int[5];
+            Random rnd = new Random();
+            MyClass A = new MyClass();        
+            Console.WriteLine(A);
+            Console.ReadKey();
         }
     }
 }
