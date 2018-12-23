83caf75428a3bce7f2ce667f393ea4860bc41a82
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-23 14:24:28 +0300

    q

--- Глава_8/Задача_1/Задача_1/Program.cs
+++ Глава_8/Задача_1/Задача_1/Program.cs
@@ -23,20 +23,20 @@ namespace Задача_1
             --a.Symbol;
             return a;
         }
-        public static int operator+(MyClass a, int b)
+        public static MyClass operator+(MyClass a, int b)
         {
-            int m = a.Symbol + b;            
-            return new MyClass((char) m);
+            char m = (char) (a.Symbol + b);            
+            return new MyClass(m);
         }
-        public static int operator +(int b, MyClass a)
+        public static MyClass operator +(int b, MyClass a)
         {
-            int m = b + a.Symbol;
-            return m;
+            char m = (char) (b + a.Symbol);
+            return new MyClass(m);
         }
-        public static int operator -(MyClass a, MyClass b)
+        public static MyClass operator -(MyClass a, MyClass b)
         {
             int m = a.Symbol - b.Symbol;
-            return m;
+            return new MyClass((char) m);
         }
         public override string ToString()
         {
@@ -49,17 +49,16 @@ namespace Задача_1
     {
         static void Main(string[] args)
         {
-            MyClass A = new MyClass('B');
-            MyClass B = new MyClass('B');
+            MyClass A = new MyClass('A');
+            MyClass B = new MyClass('Z');
             ++A;
-            --B;
-            MyClass C = A + 1;
-            int m = 5 - A;
-            C = A - B;
             Console.WriteLine(A);
+            --B;
             Console.WriteLine(B);
-            Console.WriteLine("Целочисленное поле: "+C);
-            Console.WriteLine("Целочисленное поле: " + C);
+            MyClass C = A + 1;
+            Console.WriteLine(C);
+            C = A - B;
+            Console.WriteLine(C);
             Console.ReadKey();
         }
     }
