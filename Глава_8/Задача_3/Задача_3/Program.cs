b8f58f2af0de83c64f42c79d4a54ebb4a3d75dec
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-24 19:12:02 +0300

    qwerty

---
+++ Глава_8/Задача_3/Задача_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_3
+{
+    class MyClass
+    {
+        public int Number1, Number2;
+        public MyClass(int n, int m)
+        {
+            Number1 = n;
+            Number2 = m;
+        }
+        public static bool operator >(MyClass a, MyClass b)
+        {
+            if ((Math.Pow(a.Number1, 2) + Math.Pow(a.Number2, 2)) > (Math.Pow(b.Number1, 2) + Math.Pow(b.Number2, 2))) return true;
+            else return false;
+        }
+        public static bool operator <(MyClass a, MyClass b)
+        {
+            if ((Math.Pow(a.Number1, 2) + Math.Pow(a.Number2, 2)) < (Math.Pow(b.Number1, 2) + Math.Pow(b.Number2, 2))) return true;
+            else return false;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass A = new MyClass(7, 8);
+            MyClass B = new MyClass(8, 8);
+            bool C;
+            C = A > B;
+            Console.WriteLine("A > B: " + C);
+            C = A < B;
+            Console.WriteLine("A < B: " + C);
+            Console.ReadKey();
+        }
+    }
+}
