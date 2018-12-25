d2fad791e3d28b6927a9616988073dbba1150b84
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-25 18:17:42 +0300

    5

---
+++ Глава_8/Задача_5/Задача_5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_5
+{
+    class MyClass
+    {
+        public int Number;
+        public char Symbol;
+        public MyClass(int n, char m)
+        {
+            Number = n;
+            Symbol = m;
+        }
+        public static bool operator true(MyClass a)
+        {
+            return a.Number - a.Symbol >= 10;
+        }
+        public static bool operator false(MyClass a)
+        {
+            return a.Number - a.Symbol < 10;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass A = new MyClass(78, 'A');
+            while (A)
+            {
+                Console.WriteLine(" " + A.Symbol);
+                A.Symbol++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
