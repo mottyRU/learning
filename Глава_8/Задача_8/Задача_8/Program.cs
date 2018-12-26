a176dfc5f4f346a532ec77c72ec898931d74da8c
mottyRU <44338113+mottyRU@users.noreply.github.com>
2018-12-26 19:00:04 +0300

    qwerty

---
+++ Глава_8/Задача_8/Задача_8/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_8
+{
+    class Pereopredelitel
+    {
+        public int[] Numbers =new int[5];
+        public Pereopredelitel()
+        {
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Numbers[i] = i;
+            }
+        }
+        public static implicit operator string(Pereopredelitel a)
+        {
+            string NewText = "";
+            for (int i = 0; i < a.Numbers.Length; i++)
+            {
+                NewText += a.Numbers[i];
+            }
+            return NewText;
+        }
+        public static implicit operator int(Pereopredelitel a)
+        {
+            int NewNumber = a.Numbers.Sum();
+            return NewNumber;
+        }
+        public static implicit operator Pereopredelitel(int n)
+        {
+            Pereopredelitel B = new Pereopredelitel();
+            Array.Resize(ref B.Numbers, 7);
+            for (int i = 0; i != n; i++)
+            {
+                B.Numbers[i] = 0;
+            }
+            return B;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pereopredelitel A = new Pereopredelitel();
+            string x = A;
+            Console.WriteLine("Object to String: " + x);
+            int y = A;
+            Console.WriteLine("Object to Int: " + y);
+            Pereopredelitel z = 7;
+            Console.WriteLine("Int to Object: " + z);
+            Console.ReadKey();
+        }
+    }
+}
