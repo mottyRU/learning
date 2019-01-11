c25c4ac99ffe954f01729d000e97e2461884eb5e
mottyRU <44338113+mottyRU@users.noreply.github.com>
2019-01-11 20:25:17 +0300

    qqq

---
+++ Глава_9/Задача_3/Задача_3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_3
+{
+    class MyMegaClass
+    {
+        private int[] Numbers = new int[5];
+        public int[] ZapolnMassive()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Numbers[i] = rnd.Next(0, 100);
+            }
+            return Numbers;
+        }
+        public void PokazMassive()
+        {
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Console.Write("|" + Numbers[i]);
+            }
+            Console.WriteLine("|");
+        }
+        public int CBOiCTBO
+        {
+            get
+            {
+                int Summa = Numbers.Sum();
+                return Summa;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMegaClass A = new MyMegaClass();
+            A.ZapolnMassive();
+            A.PokazMassive();
+            Console.WriteLine("Сумма элементов: " + A.CBOiCTBO);
+            Console.ReadKey();
+        }
+    }
+}
