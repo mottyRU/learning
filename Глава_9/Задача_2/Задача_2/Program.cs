e6303381c32c7a45697524d2480e788096859e96
mottyRU <44338113+mottyRU@users.noreply.github.com>
2019-01-11 20:10:00 +0300

    qq

---
+++ Глава_9/Задача_2/Задача_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    class MyNewClass
+    {
+        private int[] Numbers = new int[5] {1,2,3,4,5};
+        private int Index = 0;
+        public int CBOiCTBO
+        {
+            get
+            {
+                if (Index > 4) Index = 0;
+                Index += 1;
+                return Numbers[Index-1];
+            }
+            set
+            {
+                value = 99;
+                Numbers[Index-1] = value;
+
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyNewClass A = new MyNewClass();
+            Console.WriteLine("Значение элемента массива: " + A.CBOiCTBO);
+            Console.WriteLine("Значение элемента массива: " + A.CBOiCTBO);
+            Console.WriteLine("Значение элемента массива: " + A.CBOiCTBO);
+            A.CBOiCTBO = 99;
+            Console.WriteLine("Значение элемента массива: " + A.CBOiCTBO);
+            Console.WriteLine("Значение элемента массива: " + A.CBOiCTBO);
+            Console.WriteLine("Значение элемента массива: " + A.CBOiCTBO);
+            Console.WriteLine("Значение элемента массива: " + A.CBOiCTBO);
+            Console.WriteLine("Значение элемента массива: " + A.CBOiCTBO);
+            Console.WriteLine("Значение элемента массива: " + A.CBOiCTBO);
+            Console.WriteLine("Значение элемента массива: " + A.CBOiCTBO);
+            Console.WriteLine("Значение элемента массива: " + A.CBOiCTBO);
+            Console.ReadKey();
+        }
+    }
+}
