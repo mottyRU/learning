a01e93b72a22b8ec340c6353ecbc07df99b7e8d9
mottyRU <44338113+mottyRU@users.noreply.github.com>
2019-01-11 19:02:15 +0300

    q

---
+++ Глава_9/Задача_1/Задача_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1
+{
+    class MyNewClass
+    {
+        private char A = 'A';
+        private char B = 'Z';
+        private char num = 'a';
+        public char Symbols
+        {
+            get
+            {
+                return num;
+            }
+            set
+            {
+                if (value < A) num = A;
+                else if (value > B) num = B;
+                else num = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyNewClass obj1 = new MyNewClass();
+            Console.WriteLine("Значение свойства: " + obj1.Symbols);
+            obj1.Symbols = '!';
+            Console.WriteLine("Значение свойства: " + obj1.Symbols);
+            Console.ReadKey();
+        }
+    }
+}
