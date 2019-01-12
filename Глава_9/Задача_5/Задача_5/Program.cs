289782cdf9de3b1fa11a171c16f5cc8e64feeb8a
mottyRU <44338113+mottyRU@users.noreply.github.com>
2019-01-12 18:26:16 +0300

    5

---
+++ Глава_9/Задача_5/Задача_5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_5
+{
+    class NewClass
+    {
+        public static int Last = 2;
+        public static int Prev = 1;
+        public static int NechentZnachen
+        {
+            get
+            {
+                int Res = Prev;
+                Prev = Last + Prev;
+                return Res;
+            }
+            set
+            {
+                Prev = 1;
+                Last = 2;
+                for (int i = 2; i <=value; i++)
+                {
+                    Prev = Last + Prev;
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NewClass.NechentZnachen = 6;
+            Console.WriteLine(NewClass.NechentZnachen);
+            Console.WriteLine(NewClass.NechentZnachen);
+            NewClass.NechentZnachen = 5;
+            Console.WriteLine(NewClass.NechentZnachen);
+            //Console.WriteLine(NewClass.NechentZnachen);
+            //Console.WriteLine(NewClass.NechentZnachen);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Глава_9/Задача_5/Задача_5/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Задача_5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Задача_5")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("28acc81e-1fa6-45c9-8b7c-9a7465f8c62d")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
