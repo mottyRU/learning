5437cccb73f85295c9a498152117a127904e55fd
mottyRU <44338113+mottyRU@users.noreply.github.com>
2019-01-13 16:43:30 +0300

    6

---
+++ Глава_9/Задача_6/Задача_6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_6
+{
+    class MyyClass
+    {
+        public int[] Numbers;
+        public MyyClass(int n)
+        {
+            Numbers = new int[n];
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Numbers[i] = i;
+            }
+        }
+        public int this[int k]
+        {
+            get
+            {
+                int num = Numbers[k];
+                return num;
+            }
+            set
+            {
+                Numbers[k] = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyyClass A = new MyyClass(5);
+            Console.WriteLine(A[4]);
+            A[4] = 10;
+            Console.WriteLine(A[4]);
+            Console.ReadKey();
+        }
+    }
+}
