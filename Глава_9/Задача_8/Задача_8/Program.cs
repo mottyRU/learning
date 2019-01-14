6b0378cc857e3e7543b870df5f2f3e589d6e073c
mottyRU <44338113+mottyRU@users.noreply.github.com>
2019-01-14 19:20:16 +0300

    8

---
+++ Глава_9/Задача_8/Задача_8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_8
+{
+    class CatClass
+    {
+        public uint Number;
+        public uint Hranitel;
+        public uint this[int i]
+        {
+
+            set
+            {
+                Hranitel = value % 10;
+                if (i == 0) Hranitel += 0;
+                else if (i == 1) Hranitel += 10;
+                else if (i == 2) Hranitel += 100;
+                else if (i == 3) Hranitel += 1000;
+                else if (i == 4) Hranitel += 10000;
+            }
+        }
+        public void ShowResult()
+        {
+            Console.WriteLine(Hranitel);
+            Console.ReadKey();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CatClass A = new CatClass();
+            A[2] = 5;
+            A.ShowResult();
+        }
+    }
+}
